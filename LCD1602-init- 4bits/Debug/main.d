86cc729bf65c3c592e22c0dd3514d644943f6ad2
alfonsoredfi <38919119+alfonsoredfi@users.noreply.github.com>
2018-10-25 00:49:53 -0500

    Add files via upload

---
+++ LCD1602-init- 4bits/Debug/main.d
@@ -0,0 +1,15 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/Respaldo/Documentos\ Electronica/Programas/Code\ Composer\ 7/MSP430G2553/LCD1602-init-\ 4bits/main.h
+main.obj: C:/ti/ccs720/ccsv7/ccs_base/msp430/include/msp430g2553.h
+main.obj: C:/ti/ccs720/ccsv7/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccs720/ccsv7/tools/compiler/ti-cgt-msp430_17.6.0.STS/include/intrinsics.h
+main.obj: C:/ti/ccs720/ccsv7/tools/compiler/ti-cgt-msp430_17.6.0.STS/include/intrinsics_legacy_undefs.h
+
+../main.c: 
+D:/Respaldo/Documentos\ Electronica/Programas/Code\ Composer\ 7/MSP430G2553/LCD1602-init-\ 4bits/main.h: 
+C:/ti/ccs720/ccsv7/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccs720/ccsv7/ccs_base/msp430/include/in430.h: 
+C:/ti/ccs720/ccsv7/tools/compiler/ti-cgt-msp430_17.6.0.STS/include/intrinsics.h: 
+C:/ti/ccs720/ccsv7/tools/compiler/ti-cgt-msp430_17.6.0.STS/include/intrinsics_legacy_undefs.h: 
